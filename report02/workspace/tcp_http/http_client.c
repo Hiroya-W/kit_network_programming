090af9b1e5ad133042b481c34cc99342082ec385
Hiroya_W <e8239@g.maizuru-ct.ac.jp>
2020-05-13 21:13:30 +0900

    :sparkles: 完成

--- report02/workspace/tcp_http/http_client.c
+++ report02/workspace/tcp_http/http_client.c
@@ -17,6 +17,134 @@
 int get_cont_by_fieldname(char out[], char buf[], char word[]);
 int url_parse(char out[3][128], char in[]);
 
+int main(int argc, char *argv[]) {
+    struct hostent *server_host;
+    struct sockaddr_in server_adrs;
+
+    int tcpsock;
+    int port = 80;
+    int strsize;
+
+    char url[3][128] = {};
+    char servername[128] = {};
+    char hostname[128] = {};
+    char k_buf[BUFSIZE], s_buf[BUFSIZE], r_buf[BUFSIZE];
+
+    if (argc == 2) {
+        if (url_parse(url, argv[1]) == -1) {
+            fprintf(stderr, "URLのパースに失敗しました。\n");
+            return 1;
+        } else if (strcmp(url[1], "http") != 0) {
+            fprintf(stderr, "HTTPプロトコルにのみ対応しています。\n");
+            return 1;
+        }
+        strcpy(hostname, url[2]);
+    } else if (argc == 4) {
+        strcpy(hostname, argv[2]);
+        port = atoi(argv[3]);
+    } else {
+        fprintf(stderr, "使い方: http://example.com [proxy.host.name port]\n");
+        return 1;
+    }
+
+    fprintf(stdout, "HTTPリクエストを送信して、サーバプログラムとコンテンツのサイズを取得します。\n");
+    fprintf(stdout, "HEAD %s HTTP/1.1\n", argv[1]);
+    fprintf(stdout, "Host: %s\n\n", hostname);
+
+    /* サーバ名をアドレス(hostent構造体)に変換する */
+    if ((server_host = gethostbyname(hostname)) == NULL) {
+        fprintf(stderr, "gethostbyname()");
+        exit(EXIT_FAILURE);
+    }
+
+    /* サーバの情報をsockaddr_in構造体に格納する */
+    memset(&server_adrs, 0, sizeof(server_adrs));
+    server_adrs.sin_family = AF_INET;
+    server_adrs.sin_port = htons(port);
+    memcpy(&server_adrs.sin_addr, server_host->h_addr_list[0], server_host->h_length);
+
+    /* ソケットをSTREAMモードで作成する */
+    if ((tcpsock = socket(PF_INET, SOCK_STREAM, 0)) == -1) {
+        fprintf(stderr, "socket()");
+        exit(EXIT_FAILURE);
+    }
+
+    /* ソケットにサーバの情報を対応づけてサーバに接続する */
+    if (connect(tcpsock, (struct sockaddr *)&server_adrs, sizeof(server_adrs)) == -1) {
+        fprintf(stderr, "connect");
+        exit(EXIT_FAILURE);
+    }
+
+    /* キーボードから文字列を入力してサーバに送信 */
+    // while ((fgets(k_buf, BUFSIZE, stdin) != NULL) && k_buf[0] != '\n') { /* 空行が入力されるまで繰り返し */
+    //     strsize = strlen(k_buf);
+    //     k_buf[strsize - 1] = 0;                    /* 末尾の改行コードを消す */
+    //     snprintf(s_buf, BUFSIZE, "%s\r\n", k_buf); /* HTTPの改行コードは \r\n */
+    //
+    //     /* 文字列をサーバに送信する */
+    //     if (send(tcpsock, s_buf, strsize + 1, 0) == -1) {
+    //         fprintf(stderr, "send()");
+    //         exit(EXIT_FAILURE);
+    //     }
+    // }
+
+    /* 文字列をサーバに送信する */
+
+    /*
+     * HEAD http://example.com
+     */
+    snprintf(s_buf, BUFSIZE, "HEAD %s HTTP/1.1\r\n", argv[1]);
+    strsize = strlen(s_buf);
+    if (send(tcpsock, s_buf, strsize, 0) == -1) {
+        fprintf(stderr, "send()");
+        exit(EXIT_FAILURE);
+    }
+    /*
+     * Host: example.com
+     */
+    snprintf(s_buf, BUFSIZE, "Host: %s\r\n", hostname);
+    strsize = strlen(s_buf);
+    if (send(tcpsock, s_buf, strsize, 0) == -1) {
+        fprintf(stderr, "send()");
+        exit(EXIT_FAILURE);
+    }
+
+    /*
+     * 改行
+     */
+    send(tcpsock, "\r\n", 2, 0); /* HTTPのメソッド（コマンド）の終わりは空行 */
+
+    /* サーバから文字列を受信する */
+    if ((strsize = recv(tcpsock, r_buf, BUFSIZE - 1, 0)) == -1) {
+        fprintf(stderr, "recv()");
+        exit(EXIT_FAILURE);
+    }
+    r_buf[strsize] = '\0';
+
+    /* 受信した文字列を画面に書く */
+    // printf("%s", r_buf);
+
+    /* 
+     * fieldの内容の表示
+     */
+    char contents_of_server[BUFSIZE] = {};
+    if (get_cont_by_fieldname(contents_of_server, r_buf, "Server") != -1) {
+        printf("サーバープログラム: %s\n", contents_of_server);
+    } else {
+        fprintf(stderr, "Serverフィールドは含まれていません。\n");
+    }
+
+    char size_of_contents[BUFSIZE] = {};
+    if (get_cont_by_fieldname(size_of_contents, r_buf, "Content-Length") != -1) {
+        printf("コンテンツの大きさ: %s\n", size_of_contents);
+    } else {
+        fprintf(stderr, "Content-Lengthフィールドは含まれていません。\n");
+    }
+
+    close(tcpsock); /* ソケットを閉じる */
+    exit(EXIT_SUCCESS);
+}
+
 int url_parse(char out[3][128], char in[]) {
     const char pattern[] = "^([httpsfile]+)://([0-9a-zA-Z.-]+)/?";
     regex_t regexBuffer;
@@ -44,106 +172,13 @@ int url_parse(char out[3][128], char in[]) {
             printf("exit");
             continue;
         }
-        printf("index [start, end] = %d, %d\n", startIndex, endIndex);
+        // printf("index [start, end] = %d, %d\n", startIndex, endIndex);
         strncpy(out[i], in + startIndex, endIndex - startIndex);
-        printf("%s\n", out[i]);
+        out[i][endIndex - startIndex] = '\0';
+        // printf("%s\n", out[i]);
     }
     regfree(&regexBuffer);
     return 0;
-}
-
-int main(int argc, char *argv[]) {
-    struct hostent *server_host;
-    struct sockaddr_in server_adrs;
-
-    int tcpsock;
-    int port = 80;
-    int strsize;
-
-    char url[3][128] = {};
-    char servername[128] = {};
-    char hostname[128] = {};
-    char k_buf[BUFSIZE], s_buf[BUFSIZE], r_buf[BUFSIZE];
-
-    if (argc == 2) {
-        if (url_parse(url, argv[1]) == -1) {
-            fprintf(stderr, "URLのパースに失敗しました\n");
-        }
-        strcpy(hostname, url[2]);
-    } else if (argc == 4) {
-        strcpy(hostname, argv[2]);
-        port = atoi(argv[3]);
-    } else {
-        fprintf(stderr, "Usage: http://example.com [proxy.host.name port]\n");
-        return 1;
-    }
-
-    /* サーバ名をアドレス(hostent構造体)に変換する */
-    if ((server_host = gethostbyname(servername)) == NULL) {
-        fprintf(stderr, "gethostbyname()");
-        exit(EXIT_FAILURE);
-    }
-
-    /* サーバの情報をsockaddr_in構造体に格納する */
-    memset(&server_adrs, 0, sizeof(server_adrs));
-    server_adrs.sin_family = AF_INET;
-    server_adrs.sin_port = htons(port);
-    memcpy(&server_adrs.sin_addr, server_host->h_addr_list[0], server_host->h_length);
-
-    /* ソケットをSTREAMモードで作成する */
-    if ((tcpsock = socket(PF_INET, SOCK_STREAM, 0)) == -1) {
-        fprintf(stderr, "socket()");
-        exit(EXIT_FAILURE);
-    }
-
-    /* ソケットにサーバの情報を対応づけてサーバに接続する */
-    if (connect(tcpsock, (struct sockaddr *)&server_adrs, sizeof(server_adrs)) == -1) {
-        fprintf(stderr, "connect");
-        exit(EXIT_FAILURE);
-    }
-
-    /* キーボードから文字列を入力してサーバに送信 */
-    while ((fgets(k_buf, BUFSIZE, stdin) != NULL) && k_buf[0] != '\n') { /* 空行が入力されるまで繰り返し */
-        strsize = strlen(k_buf);
-        k_buf[strsize - 1] = 0;                    /* 末尾の改行コードを消す */
-        snprintf(s_buf, BUFSIZE, "%s\r\n", k_buf); /* HTTPの改行コードは \r\n */
-
-        /* 文字列をサーバに送信する */
-        if (send(tcpsock, s_buf, strsize + 1, 0) == -1) {
-            fprintf(stderr, "send()");
-            exit(EXIT_FAILURE);
-        }
-    }
-
-    send(tcpsock, "\r\n", 2, 0); /* HTTPのメソッド（コマンド）の終わりは空行 */
-
-    /* サーバから文字列を受信する */
-    if ((strsize = recv(tcpsock, r_buf, BUFSIZE - 1, 0)) == -1) {
-        fprintf(stderr, "recv()");
-        exit(EXIT_FAILURE);
-    }
-    r_buf[strsize] = '\0';
-
-    /* 受信した文字列を画面に書く */
-    // printf("%s", r_buf);
-
-    /* fieldの表示 */
-    char contents_of_server[BUFSIZE] = {};
-    if (get_cont_by_fieldname(contents_of_server, r_buf, "Server") != -1) {
-        printf("サーバープログラム: %s\n", contents_of_server);
-    } else {
-        fprintf(stderr, "Serverフィールドは含まれていません。\n");
-    }
-
-    char size_of_contents[BUFSIZE] = {};
-    if (get_cont_by_fieldname(size_of_contents, r_buf, "Content-Length") != -1) {
-        printf("コンテンツの大きさ: %s\n", size_of_contents);
-    } else {
-        fprintf(stderr, "Content-Lengthフィールドは含まれていません。\n");
-    }
-
-    close(tcpsock); /* ソケットを閉じる */
-    exit(EXIT_SUCCESS);
 }
 
 int get_cont_by_fieldname(char out[], char buf[], char word[]) {
