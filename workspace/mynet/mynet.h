ca4f8f77468175bde3232b78bad7829f958e9b1a
Hiroya_W <e8239@g.maizuru-ct.ac.jp>
2020-07-13 11:51:43 +0900

    :+1: broadcast用のinit関数をヘッダに追加した

--- workspace/mynet/mynet.h
+++ workspace/mynet/mynet.h
@@ -20,6 +20,8 @@ int init_udpserver(in_port_t myport);
 int init_udpclient();
 void set_sockaddr_in(struct sockaddr_in *server_adrs, char *servername,
                      in_port_t port_number);
+void set_sockaddr_in_broadcast(struct sockaddr_in *server_adrs,
+                               in_port_t port_number);
 int Sendto(int sock, const void *s_buf, size_t strsize, int flags,
            const struct sockaddr *to, socklen_t tolen);
 int Recvfrom(int sock, void *r_buf, size_t len, int flags,
