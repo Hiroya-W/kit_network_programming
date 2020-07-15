6d076239af62ed7ce7dcfd871799f727680d39d4
Hiroya_W <e8239@g.maizuru-ct.ac.jp>
2020-07-15 14:11:27 +0900

    :+1: サーバーに参加する部分を関数にした

--- workspace/idobata2/idobata_client.c
+++ workspace/idobata2/idobata_client.c
@@ -4,34 +4,35 @@
 #include "idobata.h"
 #include "mynet.h"
 
-WINDOW *win_main, *win_sub;
+static WINDOW *win_main, *win_sub;
 
-static int init(int port_number);
-static void show_client_mode_message();
+int join_server(int port_number);
 
 void idobata_client(int port_number) {
     int sock;
     /* Windowを作成 */
     create_window(&win_main, &win_sub);
+    wprintw(win_main, "クライアントモードで起動しました。\n");
     /* クライアント用の初期設定を行う*/
-    sock = init(port_number);
+    sock = join_server(port_number);
+    /* 画面更新 */
+    wprintw(win_main, "サーバーに参加しました。\n");
+    wrefresh(win_main);
+    wrefresh(win_sub);
 }
 
-static int init(int port_number) {
+int join_server(int port_number) {
     char server_addr[20];
     char user_name[USERNAME_LEN];
     char s_buf[MSGBUF_SIZE];
     int sock;
     int strsize;
-    /* クライアントモードで起動した時に最初に表示するメッセージ */
-    show_client_mode_message();
     /* 格納されているサーバーアドレスを取得する */
     get_server_addr(server_addr);
     /* 格納されているユーザ名を取得する */
     get_user_name(user_name);
     /* サーバーに接続する */
     sock = init_tcpclient(server_addr, port_number);
-    wprintw(win_main, "サーバーに接続しました。");
     /* JOINパケットを送信する */
     create_packet(s_buf, JOIN, user_name);
     strsize = strlen(s_buf);
@@ -39,7 +40,3 @@ static int init(int port_number) {
     return sock;
 }
 
-/* クライアントモードで起動した時に最初に表示するメッセージ */
-static void show_client_mode_message() {
-    wprintw(win_main, "クライアントモードで起動します。\n");
-}
